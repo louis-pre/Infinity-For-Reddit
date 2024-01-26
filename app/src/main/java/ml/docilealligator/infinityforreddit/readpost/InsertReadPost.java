6110e4c775b94d7f7afe5ea840c7780baa16cd25
Louis Pré <louispre94@gmail.com>
2024-01-25 16:04:16 -0800

    Remove read post limit

--- app/src/main/java/ml/docilealligator/infinityforreddit/readpost/InsertReadPost.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/readpost/InsertReadPost.java
@@ -8,11 +8,8 @@ public class InsertReadPost {
     public static void insertReadPost(RedditDataRoomDatabase redditDataRoomDatabase, Executor executor,
                                       String username, String postId) {
         executor.execute(() -> {
-            ReadPostDao readPostDao = redditDataRoomDatabase.readPostDao();
-            if (readPostDao.getReadPostsCount(username) > 500) {
-                readPostDao.deleteOldestReadPosts(username);
-            }
             if (username != null && !username.equals("")) {
+                ReadPostDao readPostDao = redditDataRoomDatabase.readPostDao();
                 readPostDao.insert(new ReadPost(username, postId));
             }
         });
